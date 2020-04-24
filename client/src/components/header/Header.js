b8eb97eb64608656f3813343386957121d0edd24
dms2366 <dms2366@naver.com>
2020-04-24 23:37:28 +0900

    heaer 수정

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,9 +2,16 @@ import React from "react";
 import css from "./Header.css";
 
 const Header = () => {
-  return (<header>
-      <span>header</span>
-  </header>);
+  return (
+    <header>
+        <div className="logo">
+            MOMATO
+        </div>
+        <div class="login">
+            로그인 / 회원가입
+        </div>
+    </header>
+);
 };
 
 export default Header;
