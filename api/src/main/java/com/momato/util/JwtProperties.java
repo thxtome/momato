e0453971f216b5e6eed9dd349e85137a5b2ccd7e
thxtome <thxtome531@gmail.com>
2020-05-23 23:35:53 +0900

    로그인 지속 시간 100일로 수정

--- api/src/main/java/com/momato/util/JwtProperties.java
+++ api/src/main/java/com/momato/util/JwtProperties.java
@@ -2,7 +2,7 @@ package com.momato.util;
 
 public class JwtProperties {
 	public static final String SECRET = "Secret";
-    public static final long EXPIRATION_TIME = 86400000*30; // 30 days
+    public static final long EXPIRATION_TIME =  (8640000000L); // 100 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
