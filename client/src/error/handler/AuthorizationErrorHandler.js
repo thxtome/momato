448e635590f18e61af9c19f8189abe3301d02a49
dms2366 <dms2366@naver.com>
2020-07-06 18:41:59 +0900

    토큰 유효기간 지날 시 저장된 토큰 삭제

--- client/src/error/handler/AuthorizationErrorHandler.js
+++ client/src/error/handler/AuthorizationErrorHandler.js
@@ -3,9 +3,11 @@ import { toast } from 'react-toastify';
 const AuthorizationErrorHandler = errorDetail => {
   switch (errorDetail.errorCode) {
     case '0001':
-      toast.error(`로그인이 되어있지 않거나 유효하지 않은 토큰입니다.`, {
-        position: toast.POSITION.TOP_CENTER,
-      });
+      // 토큰 기간이 지나면 localStorage에 저장된 토큰 삭제
+      localStorage.removeItem('auth');
+      // toast.error(`로그인이 되어있지 않거나 유효하지 않은 토큰입니다.`, {
+      //   position: toast.POSITION.TOP_CENTER,
+      // });
       break;
   }
 };
