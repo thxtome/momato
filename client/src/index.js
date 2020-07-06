3a4feed1081a95a2d50fc7d4353ad569f963b455
dms2366 <dms2366@naver.com>
2020-07-06 21:03:16 +0900

    정선동강체 전체 적용

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'JSDongkang-Regular',
+  },
+});
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.register();
