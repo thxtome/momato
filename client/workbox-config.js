71463a3a1f0b6ef3aba4b0801a2a3fc0bcdf7b25
thxtome <thxtome531@gmail.com>
2020-07-14 11:51:07 +0900

    workbox 추가 및 설정

---
+++ client/workbox-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: ['**/*.{json,html,js,css}'],
+  swDest: 'build/sw.js',
+  runtimeCaching: [
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|mp3)$/,
+
+      // Apply a cache-first strategy.
+      handler: 'CacheFirst',
+
+      options: {
+        // Use a custom cache name.
+        cacheName: 'images',
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    },
+  ],
+};
